5408b270160e54ba12549e94a0cebc8e2507d2b5
devmaaa <dev.manucharyan@gmail.com>
2024-05-11 12:19:43 +0400

    Initializing project using Vite -Add Roboto font family -implement atomic folder structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit recusandae modi veritatis, laudantium quidem amet.</p>
+    </>
+  );
+}
+
+export default App;
